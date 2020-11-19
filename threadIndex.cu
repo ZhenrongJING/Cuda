967d150cf253a02f506c038e30d7458b43344e66
ZhenrongJING <Zhenrong.Jing@ec-nantes.fr>
2020-11-19 22:08:58 +0100

    first commit

---
+++ threadIndex.cu
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+__global__ void showCpy(float* d_a, int const nx, int const ny){
+    unsigned int i = threadIdx.x;
+    if(i<nx*ny) 
+        printf("%.2f ", d_a[i]); 
+}
+
+int main(){
+    int const nx = 1<<4;
+    int const ny = 1<<4;
+
+    float h_a[nx][ny];
+    for(int i=0; i<nx; i++){
+        for(int j=0; j<ny; j++){
+            h_a[i][j] = (float)i + ((float)j)/100;
+        }
+    }
+
+    float* d_a;
+    size_t mSize = nx*ny*sizeof(float);
+    cudaMalloc((void**)&d_a, mSize); 
+
+    cudaMemcpy(d_a, h_a, mSize, cudaMemcpyHostToDevice);
+
+    dim3 grid(1, 1);
+    dim3 block(nx*ny, 1);
+
+    showCpy<<<grid, block>>>(d_a, nx, ny);
+
+/*    for(int i=0; i<nx; i++){
+        for(int j=0; j<ny; j++){
+           printf("%5.2f ", h_a[i][j]); 
+        }
+        printf("\n");
+    }
+*/
+
+    return 0;
+}
